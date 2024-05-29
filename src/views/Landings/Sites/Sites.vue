28e42889356f21ce5b04f6e19231def74ba0d864
Felipe Carvalho <tsh.techskillhub@gmail.com>
2024-05-29 11:07:17 -0300

    Landing Sites

--- src/views/Landings/Sites/Sites.vue
+++ src/views/Landings/Sites/Sites.vue
@@ -1,93 +1,156 @@
 <template>
   <div id="lp-sites" class="container-fluid px-0">
-    <div class="header py-3">
-      <div class="container d-flex justify-content-between align-items-center">
-        <div class="logo">
-          <h2>Tech Skill Hub</h2>
-          <span>Software House</span>
-        </div>
-        <div class="time text-end">
-          <h5 class="lh-sm">A CONDIÇÃO ESPECIAL<br/>ENCERRA EM:</h5>
-          <vue-countdown :time="endTime" v-slot="{ hours, minutes, seconds }">
-            <div class="square">
-              {{ hours }}
-            </div>
-            :
-            <div class="square">
-              {{ minutes.toString().padStart(2, '0') }} 
-            </div>
-            :
-            <div class="square">
-              {{ seconds.toString().padStart(2, '0') }}
-            </div>
-          </vue-countdown>
-        </div>
-      </div>  
-    </div>
     <div class="banner">
       <div class="container">
         <div class="row">
-          <div class="col-7 pt-5 splash">
-            <h1>NÃO PERCA TEMPO!!!</h1>
-            <h2>TENHA SEU SITE</h2>
+          <div class="col-12 col-lg-5 pt-5 content">
+            <h1 class="logo">
+              Tech Skill Hub
+              <span>Software House</span>
+            </h1>
+            <h3 class="text-center mt-4">Tenha seu <span>site profissional</span> agora mesmo!</h3>
             <div class="box mt-4">
               <div class="list">
-                <i class="fa-regular fa-circle-check"></i> Presença Online 24/7
-                <i class="fa-regular fa-circle-check"></i> Credibilidade e Profissionalismo
-                <i class="fa-regular fa-circle-check"></i> Marketing e Promoção
-                <i class="fa-regular fa-circle-check"></i> Informação e Atendimento ao Cliente
-                <i class="fa-regular fa-circle-check"></i> Análise e Melhorias Contínuas
+                <!-- <i class="fa-regular fa-circle-check"></i> Layout exclusivo
+                <i class="fa-regular fa-circle-check"></i> Equipe qualificada
+                <i class="fa-regular fa-circle-check"></i> Suporte completo
+                <i class="fa-regular fa-circle-check"></i> Alta performance
+                <i class="fa-regular fa-circle-check"></i> Tecnologias modernas -->
+                <i class="fa-regular fa-circle-check"></i> Presença <span>online 24h/dia</span> todos os dias
+                <i class="fa-regular fa-circle-check"></i> Mais credibilidade e <span>profissionalismo</span>
+                <i class="fa-regular fa-circle-check"></i> <span>Design personalizado</span> e domínio próprio
+                <i class="fa-regular fa-circle-check"></i> Estreita sua <span>relação com os clientes</span>
+                <i class="fa-regular fa-circle-check"></i> <span>Análise de dados</span> e melhorias contínuas
               </div>
-              <Form>
-                <small>
-                  Deixe seu e-mail e um de nossos consultores entrará em contato assim que possível.
-                </small>
-                <Input
-                  idInput="email"
-                  nameInput="e-mail"
-                  typeInput="email"
-                  rulesInput="required|email"
-                  placeholderInput="Deixe seu melhor e-mail."
-                  @value="(i) => i.value.length >= 0 ? form.email = i.value : form.email"
-                  :valueInput="form.email"
-                />
-                
-                <button class="primary ms-auto" @click.prevent="submit()">
-                  Quero meu site!
-                </button>
-              </Form>
+              <div class="contact mt-4">
+                Entre em contato <br class="d-lg-none"/>pelo WhatsApp
+                <img src="./imgs/whatsapp.svg" alt="">
+              </div>
             </div>
           </div>
+          <div class="col-12 col-lg-7 d-flex align-items-center">
+            <img src="./imgs/bg.webp" class="img-fluid mt-5" alt="">
+          </div>
         </div>
-        <div class="row mt-3">
-          <div class="col-12 splash">
+        <!-- <div class="row mt-3">
+          <div class="col-12 content">
             <div class="price">
               <small>POR APENAS</small>
               <h4>Entrada +</h4>
               <h2 style="margin-top: -30px;"><span>12x de</span> R$240</h2>
             </div>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
+
+    <div class="perks">
+      <div class="top pt-4">
+      <h2 class="text-center">Por que investir em um <span>site profissional?</span></h2>
+      </div>
+      <div class="col-10 col-lg-7 mx-auto boxMain p-3 p-lg-5">
+        <h6 class="text-center mb-0">
+          Ter um site profissional é mais do que estar na internet. É uma forma de passar confiança, melhorar a experiência dos visitantes, ser encontrado mais facilmente e fazer marketing de forma mais eficaz. Tudo isso ajuda a atrair mais clientes e aumentar as vendas.
+        </h6>
+      </div>
+
+      <div class="container mx-auto mt-5">
+        <div class="row">
+          <div class="col-12 col-lg-7">
+            <div class="boxItem">
+              <h5>Credibilidade e Imagem Profissional</h5>
+              <p class="mb-0">
+                Um site bem feito mostra que a empresa é séria, confiável e profissional. Se o site for amador ou desatualizado, o cliente pode duvidar da capacidade e competência da empresa. Um site bonito, organizado e atualizado transmite uma imagem de confiança, aumentando a chance de atrair e manter clientes.
+              </p>
+            </div>
+          </div>
+          <div class="col-12 col-lg-5 mt-4 mt-lg-0">
+            <div class="boxItem">
+              <h5>Melhor Experiência do Usuário (UX)</h5>
+              <p class="mb-0">
+                Um site profissional é fácil de navegar, carrega rápido e funciona bem em qualquer dispositivo, seja celular ou computador. Quando o site é agradável de usar, os visitantes ficam mais tempo e têm mais chances de virar clientes.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-12 col-lg-5">
+            <div class="boxItem">
+              <h5>Otimização para Sites de Busca (SEO)</h5>
+              <p class="mb-0">
+                Um site feito por profissionais segue as melhores práticas de SEO, ou seja, ele é construído para aparecer bem nos resultados de busca. Isso significa mais visibilidade e mais visitantes no site, o que aumenta as chances de venda.
+              </p>
+            </div>
+          </div>
+          <div class="col-12 col-lg-7 mt-4 mt-lg-0">
+            <div class="boxItem">
+              <h5>Integração com Ferramentas de Marketing Digital</h5>
+              <p class="mb-0">
+                Um site profissional pode ser integrado com várias ferramentas de marketing, como e-mail marketing e redes sociais. Isso facilita a gestão das campanhas, permite entender melhor o comportamento dos visitantes e criar estratégias mais eficientes.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <button class="mx-auto mt-5">QUERO MEU SITE PROFISSIONAL!</button>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="about pt-5">
+      <h1 class="text-center">SEU SITE <span>pensado</span> PARA VOCÊ!</h1>
+      <div class="container">
+        <div class="row mt-5">
+          <div class="col-12 col-lg">
+            <div class="box p-4">
+              <img src="./imgs/iconCalendar.svg" alt="Menor prazo">
+              <span>Menor prazo</span>
+              Sabemos que tempo é dinheiro. Por isso, na Tech Skill Hub, nos comprometemos a entregar seu site em um prazo curto, sem abrir mão da qualidade. Confie na nossa equipe para obter um site funcional e atrativo rapidamente, e ganhe vantagem no mercado!
+            </div>
+          </div>
+          <div class="col-12 col-lg">
+            <div class="box p-4">
+              <img src="./imgs/iconTag.svg" alt="Preço justo">
+              <span>Preço justo</span>
+              Nós da TSH, acreditamos que um site profissional não precisa custar uma fortuna. Oferecemos preços justos e competitivos para garantir que você tenha um site de alta qualidade que se encaixe no seu orçamento. Invista no seu negócio sem comprometer suas finanças!
+            </div>
+          </div>
+          <div class="col-12 col-lg">
+            <div class="box p-4">
+              <img src="./imgs/iconQuality.svg" alt="Alta qualidade">
+              <span>Alta qualidade</span>
+              A qualidade é nossa prioridade. Na Tech Skill Hub, utilizamos as melhores práticas e tecnologias para criar sites que não apenas atendem, mas superam suas expectativas. Experimente a diferença que um site de qualidade superior pode fazer para sua presença online.
+            </div>
+          </div>
+          <!-- <div class="col-12 col-lg">
+            <div class="box p-4">
+              <img src="./imgs/iconValue.svg" alt="Valor agregado">
+              <span>Valor agregado</span>
+              Oferecemos mais do que apenas um site. Na Tech Skill Hub, criamos soluções digitais que agregam valor real ao seu negócio. Nossos sites são projetados para aumentar seu alcance, engajar clientes e impulsionar suas vendas. Invista em um site que trabalha para você!
+            </div>
+          </div> -->
+        </div>
+
+        <div class="row">
+          <button class="mx-auto mt-lg-5 mt-3">VAMOS FAZER MEU SITE!</button>
+        </div>
+
+      </div>
+    </div>
+
+    <Footer/>
   </div>
 </template>
 <script>
-import Input from './../../../components/form/Input.vue'
+import Footer from './../../../common/footer/Footer.vue'
 export default {
   data() {
-    const now = new Date();
-    const tomorrow = new Date('2024, 5, 22 : 10:00:00');
     
-    return {
-      endTime: tomorrow-now,
-      form: {
-        email: ''
-      }
-    }
   },
   components: {
-    Input
+    Footer
   }
   
 }
@@ -95,147 +158,262 @@ export default {
 <style lang="scss" scoped>
 #lp-sites {
   min-height: 100vh;
-  background: $dark;
+  background: $grey3;
   margin-top: -100px;
-  .header {
-    background-color: rgb($grey1, 0.7);
-    border-bottom: 3px solid $primary;
-    position: fixed;
-    width: 100%;
-    .logo {
-      width: fit-content;
-      h2 {
+  .banner {
+    background-image: url('./imgs/bg_top.webp');
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: cover;
+    padding-bottom: 2rem;
+    .content {
+      h1.logo {
         color: $secondary;
         font-family: $fontLogo;
         line-height: 100%;
-      }
-      span {
-        font-family: 'Montserrat', sans-serif;
-        background-color: $primary;
-        color: $black;
-        border-radius: 2rem;
-        padding: 0.2rem 0.75rem;
-        font-weight: 700;
-        display: block;
         width: fit-content;
-        margin-left: auto;
-        margin-top: -5px;
-        font-size: 0.8rem;
+        font-size: 4rem;
+        margin: auto;
+        span {
+          font-size: 1.25rem;
+          font-family: 'Montserrat', sans-serif;
+          background-color: $primary;
+          color: $black;
+          border-radius: 2rem;
+          padding: 0.25rem 0.75rem;
+          font-weight: 700;
+          display: block;
+          width: fit-content;
+          margin-left: auto;
+          margin-top: 0px;
+          line-height: normal;
+        }
       }
-    }
-    .time {
-      display: flex;
-      align-items: center;
-      h5 {
-        margin-bottom: 0;
-        color: $secondary;
+      h3 {
+        line-height: normal;
+        span {
+          color: $primary;
+        }
       }
-      .square {
-        background-color: $primary;
-        border-radius: 8px;
-        border: 2px solid $secondary;
-        padding: 0.75rem 0.5rem;
-        width: 60px;
-        text-align: center;
-      }
-      span {
-        color: $white;
-        font-weight: 700;
-        font-size: 2rem;
-        display: flex;
-        margin-left: 1rem;
-        justify-content: center;
-        gap: 5px;
-        align-items: baseline;
-      }
-    }
-  }
-  .banner {
-    min-height: 80vh;
-    padding-top: 100px;
-    background-color: white;
-    background-image: url('./imgs/websites.png');
-    background-repeat: no-repeat;
-    background-position: center right;
-    background-size: 55%;
-    .splash {
       .box {
-        background-color: $dark;
+        background-color: rgb($offWhite, 0.3);
         border-radius: 0.5rem;
         padding: 2rem;
-        width: fit-content;
+        width: 100%;
+        margin: auto;
         .list {
-          color: $white;
+          color: $dark;
           display: grid;
           grid-template-columns: auto auto;
-          gap: 0.75rem 0.5rem;
+          gap: 1rem 0.5rem;
           align-items: center;
-          font-size: 1.5rem;
+          font-size: 1.25rem;
+          font-weight: 500;
+          span, i {
+            font-weight: 900;
+            color: $primary;
+            display: contents;
+          }
+        }
+        .contact {
+          background-color: $success;
+          color: white;
+          line-height: normal;
+          padding: 1.25rem 1rem;
+          border-radius: 0.5rem;
+          font-weight: 600;
+          position: relative;
+          font-size: 1.15rem;
+          img {
+            width: 80px;
+            position: absolute;
+            top: -10px;
+            right: 0.5rem;
+          }
         }
       }
-      .price {
-        width: fit-content;
-        margin: auto;
-      }
-      h1 {
-        font-size: 3rem;
-        line-height: 100%;
-        color: red;    
-        font-weight: 900;
-        text-shadow:
-          5px 5px 0 yellow,
-          -2px -2px 0 yellow,  
-          2px -2px 0 yellow,
-          -2px 2px 0 yellow,
-          2px 2px 0 yellow;
-        // transform: rotate(-5deg);
-        // margin-left: 200px;
-      }
-      h2 {
-        font-size: 4.5rem;
-        line-height: 100%;
-        font-weight: 900;
-        color: $primary-light;
-        text-shadow:
-          5px 5px 0 $secondary,
-          -2px -2px 0 $secondary,  
-          2px -2px 0 $secondary,
-          -2px 2px 0 $secondary,
-          2px 2px 0 $secondary;
-        // transform: rotate(-5deg);
-        span {
-          vertical-align: text-bottom;
+      // .price {
+      //   width: fit-content;
+      //   margin: auto;
+      //   h2 {
+      //     font-size: 4.5rem;
+      //     line-height: 100%;
+      //     font-weight: 900;
+      //     color: $primary-light;
+      //     text-shadow:
+      //       5px 5px 0 $secondary,
+      //       -2px -2px 0 $secondary,  
+      //       2px -2px 0 $secondary,
+      //       -2px 2px 0 $secondary,
+      //       2px 2px 0 $secondary;
+      //     span {
+      //       vertical-align: text-bottom;
+      //       font-size: 2.5rem;
+      //     }
+      //   }
+      // }
+    }
+    @media (max-width: $lg) {
+      .content {
+        h1.logo {
           font-size: 2.5rem;
+          span {
+            font-size: 0.75rem;
+          }
+        }
+        h3 {
+          font-size: 1.5rem;
+        }
+        .box {
+          .list {
+            align-items: flex-start;
+            gap: 1.5rem 0.5rem;
+            i::before {
+              margin-top: 3px;
+            }
+          }
+          .contact {
+            img {
+              top: 3px;
+            }
+          }
         }
       }
     }
   }
-}
-</style>
-<style lang="scss">
-#lp-sites {
-  .splash {
+
+  .perks {
+    padding-bottom: 200px;
+    background-image: url('./imgs/section2.webp');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center bottom;
+    background-color: white;
+    .top {
+      height: 200px;
+      background: rgb(128,0,255);
+      background: linear-gradient(0deg, rgba(128,0,255,1) 0%, rgba(70,0,139,1) 100%);
+      h2 {
+        color: white;
+        span {
+          color: $primary-light;
+          // text-decoration: underline;
+          text-transform: uppercase;
+          font-weight: 900;
+        }
+      }
+    }
+    .boxMain {
+      border-radius: 1rem;
+      background-color: $white;
+      margin-top: -100px;
+      min-height: 200px;
+      box-shadow: 0px 0px 15px 5px black;
+      color: $grey3;
+    }
+    .boxItem {
+      background-color: $primary-light;
+      height: 100%;
+      padding: 1rem 1.5rem;
+      border-radius: 0.5rem;
+      // box-shadow: 0px 0px 10px 0px black;
+      h5 {
+        color: $secondary;
+      }
+      p {
+        color: white;
+      }
+    }
+    button {
+      background: rgb(128,0,255);
+      background: linear-gradient(180deg, rgba(128,0,255,1) 0%, rgba(70,0,139,1) 100%);
+      color: white;
+      font-size: 1.75rem;
+      border-radius: 1rem;
+      padding: 1rem 2.5rem;
+      outline: none;
+      border: 5px solid $secondary-light;
+      transition: all 0.5s ease-in-out;
+      font-weight: 700;
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+    @media (max-width: $lg) {
+      background-size: 170%;
+      padding-bottom: 100px;
+      .top {
+        h2 {
+          font-size: 1.75rem;
+        }
+      }
+      .boxMain {
+        margin-top: -70px;
+        h6 {
+          font-size: 1rem;
+        }
+      }
+      button {
+        font-size: 1rem;
+      }
+    }
+  }
+  
+  .about {
+    background-color: $offWhite;
+    min-height: 50vh;
+    padding-bottom: 50PX;
+    // background: rgb(0,190,190);
+    // background: linear-gradient(0deg, rgba(0,190,190,1) 0%, rgba(0,139,139,1) 100%);
+    h1 {
+      color: $secondary;
+      font-weight: 900;
+      font-size: 3.5rem;
+      span {
+        color: $dark;
+        font-weight: 700;
+      }
+    }
     .box {
-      form {
-        margin-top: 1rem;  
-        small {
-          color: white;
-          max-width: 470px;
-          display: block;
-          margin-bottom: 0.5rem;
-          margin-left: 0.5rem;
-          margin-top: 1.5rem;
-        }
-        input {
-          border-radius: 25px;
-          min-height: auto;
-          border: none;
-          padding: 8px 15px;
-          margin-bottom: 0
-        }
-        button {
-          border-radius: 25px;
-        }
+      background-color: white;
+      border-radius: 0.5rem;
+      text-align: center;
+      img {
+        width: 100px;
+        margin-bottom: 1rem;
+      }
+      span {
+        font-weight: 600;
+        font-size: 1.25rem;
+        margin-bottom: 0.5rem;
+        display: block;
+      }
+    }
+    button {
+      // background: rgb(128,0,255);
+      // background: linear-gradient(180deg, rgba(128,0,255,1) 0%, rgba(70,0,139,1) 100%);
+      background: linear-gradient(180deg, rgba(0,190,190,1) 0%, rgba(0,139,139,1) 100%);
+      color: white;
+      font-size: 1.75rem;
+      border-radius: 1rem;
+      padding: 1rem 2.5rem;
+      outline: none;
+      border: 5px solid $primary-light;
+      transition: all 0.5s ease-in-out;
+      font-weight: 700;
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+    @media (max-width: $lg) {
+      h1 {
+        font-size: 2rem;
+      }
+      .box {
+        margin-bottom: 1rem;
+      }
+      button {
+        font-size: 1rem;
       }
     }
   }
---
+++ src/views/Landings/Sites/SitesOld.vue
@@ -0,0 +1,255 @@
+<template>
+  <div id="lp-sites" class="container-fluid px-0">
+    <div class="header py-3">
+      <div class="container d-flex justify-content-between align-items-center">
+        <div class="logo">
+          <h2>Tech Skill Hub</h2>
+          <span>Software House</span>
+        </div>
+        <div class="time text-end">
+          <h5 class="lh-sm">A CONDIÇÃO ESPECIAL<br/>ENCERRA EM:</h5>
+          <vue-countdown :time="endTime" v-slot="{ hours, minutes, seconds }">
+            <div class="square">
+              {{ hours }}
+            </div>
+            :
+            <div class="square">
+              {{ minutes.toString().padStart(2, '0') }} 
+            </div>
+            :
+            <div class="square">
+              {{ seconds.toString().padStart(2, '0') }}
+            </div>
+          </vue-countdown>
+        </div>
+      </div>  
+    </div>
+    <div class="banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-7 pt-5 content">
+            <h1>NÃO PERCA TEMPO!!!</h1>
+            <h2>TENHA SEU SITE</h2>
+            <div class="box mt-4">
+              <div class="list">
+                <i class="fa-regular fa-circle-check"></i> Presença Online 24/7
+                <i class="fa-regular fa-circle-check"></i> Credibilidade e Profissionalismo
+                <i class="fa-regular fa-circle-check"></i> Marketing e Promoção
+                <i class="fa-regular fa-circle-check"></i> Informação e Atendimento ao Cliente
+                <i class="fa-regular fa-circle-check"></i> Análise e Melhorias Contínuas
+              </div>
+              <Form>
+                <small>
+                  Deixe seu e-mail e um de nossos consultores entrará em contato assim que possível.
+                </small>
+                <Input
+                  idInput="email"
+                  nameInput="e-mail"
+                  typeInput="email"
+                  rulesInput="required|email"
+                  placeholderInput="Deixe seu melhor e-mail."
+                  @value="(i) => i.value.length >= 0 ? form.email = i.value : form.email"
+                  :valueInput="form.email"
+                />
+                
+                <button class="primary ms-auto" @click.prevent="submit()">
+                  Quero meu site!
+                </button>
+              </Form>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="row mt-3">
+          <div class="col-12 content">
+            <div class="price">
+              <small>POR APENAS</small>
+              <h4>Entrada +</h4>
+              <h2 style="margin-top: -30px;"><span>12x de</span> R$240</h2>
+            </div>
+          </div>
+        </div> -->
+      </div>
+      <div class="gradient">
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Input from './../../../components/form/Input.vue'
+export default {
+  data() {
+    const now = new Date();
+    const tomorrow = new Date('2024, 5, 22 : 10:00:00');
+    
+    return {
+      endTime: tomorrow-now,
+      form: {
+        email: ''
+      }
+    }
+  },
+  components: {
+    Input
+  }
+  
+}
+</script>
+<style lang="scss" scoped>
+#lp-sites {
+  min-height: 100vh;
+  background: $grey3;
+  margin-top: -100px;
+  
+  .header {
+    // background-color: rgb($grey1, 0.7);
+    // border-bottom: 3px solid $primary;
+    position: fixed;
+    width: 100%;
+    .logo {
+      width: fit-content;
+      h2 {
+        color: $secondary;
+        font-family: $fontLogo;
+        line-height: 100%;
+      }
+      span {
+        font-family: 'Montserrat', sans-serif;
+        background-color: $primary;
+        color: $black;
+        border-radius: 2rem;
+        padding: 0.2rem 0.75rem;
+        font-weight: 700;
+        display: block;
+        width: fit-content;
+        margin-left: auto;
+        margin-top: -5px;
+        font-size: 0.8rem;
+      }
+    }
+    .time {
+      display: flex;
+      align-items: center;
+      h5 {
+        margin-bottom: 0;
+        color: $secondary;
+      }
+      .square {
+        background-color: $primary;
+        border-radius: 8px;
+        border: 2px solid $secondary;
+        padding: 0.75rem 0.5rem;
+        width: 60px;
+        text-align: center;
+      }
+      span {
+        color: $white;
+        font-weight: 700;
+        font-size: 2rem;
+        display: flex;
+        margin-left: 1rem;
+        justify-content: center;
+        gap: 5px;
+        align-items: baseline;
+      }
+    }
+  }
+  .banner {
+    min-height: 80vh;
+    padding-top: 100px;
+    background-image: url('./imgs/bg_top.webp');
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: cover;
+    min-height: 100vh;
+    
+    // background-color: white;
+    // background-image: url('./imgs/websites.png');
+    // background-repeat: no-repeat;
+    // background-position: center right;
+    // background-size: 55%;
+    .content {
+      .box {
+        background-color: $dark;
+        border-radius: 0.5rem;
+        padding: 2rem;
+        width: fit-content;
+        .list {
+          color: $white;
+          display: grid;
+          grid-template-columns: auto auto;
+          gap: 0.75rem 0.5rem;
+          align-items: center;
+          font-size: 1.5rem;
+        }
+      }
+      .price {
+        width: fit-content;
+        margin: auto;
+      }
+      h1 {
+        font-size: 3rem;
+        line-height: 100%;
+        color: white;    
+        font-weight: 900;
+        text-shadow:
+          5px 5px 0 red,
+          -2px -2px 0 red,  
+          2px -2px 0 red,
+          -2px 2px 0 red,
+          2px 2px 0 red;
+        // transform: rotate(-5deg);
+        // margin-left: 200px;
+      }
+      h2 {
+        font-size: 4.5rem;
+        line-height: 100%;
+        font-weight: 900;
+        color: $primary-light;
+        text-shadow:
+          5px 5px 0 $secondary,
+          -2px -2px 0 $secondary,  
+          2px -2px 0 $secondary,
+          -2px 2px 0 $secondary,
+          2px 2px 0 $secondary;
+        // transform: rotate(-5deg);
+        span {
+          vertical-align: text-bottom;
+          font-size: 2.5rem;
+        }
+      }
+    }
+    
+  }
+}
+</style>
+
+<style lang="scss">
+#lp-sites {
+  .content {
+    .box {
+      form {
+        margin-top: 1rem;  
+        small {
+          color: white;
+          max-width: 470px;
+          display: block;
+          margin-bottom: 0.5rem;
+          margin-left: 0.5rem;
+          margin-top: 1.5rem;
+        }
+        input {
+          border-radius: 25px;
+          min-height: auto;
+          border: none;
+          padding: 8px 15px;
+          margin-bottom: 0
+        }
+        button {
+          border-radius: 25px;
+        }
+      }
+    }
+  }
+}
+</style>
